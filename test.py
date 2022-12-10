9fe6db357965a8bd782b75b2bd37ca3fc3a49a2c
joeklug <120263338+joeklug@users.noreply.github.com>
2022-12-10 09:26:58 -0600

    Update test.py
    add terminator

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import time
 def main():
     s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     s.connect("/tmp/blahsock")
-    s.send(b"Test")
+    s.send(b"Test\0")
     print(s.recv(1024))
     s.close()
 
